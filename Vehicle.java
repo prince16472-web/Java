5cc68e4310229764a7302f92562e0ad7bd1954b8
prince16472-web <prince16472@gmail.com>
2026-05-17 22:11:03 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,26 @@
+ abstract class vehicle {
+    int No_Of_Wheels;
+     abstract void start();
+}
+class Car extends vehicle{
+    public Car() {
+        No_Of_Wheels = 4;
+    }
+    void start(){
+        System.out.println("Car is starting");   
+    }
+    class scooter extends vehicle{
+        public scooter() {
+            No_Of_Wheels = 2;
+        }
+        void start(){
+            System.out.println("Scooter is starting");
+        }
+    }
+    class Abstract_Example{
+        public static void main(String[] args) {
+            vehicle myCar = new Car();
+            myCar.start();
+        }
+    }
+}
